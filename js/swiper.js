a46dc6231bd45defbb5bda3ec8921621d4e332d2
AisErzhan <aisaev.erzhan@gmail.com>
2023-05-06 16:45:34 +0600

    upload

---
+++ js/swiper.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+ 
+  const prodSlider = $('.products__slider');
+  $('.products__slider').owlCarousel({
+      items: 2,
+      margin: 10,
+      loop: true, 
+      dots: false,
+      autoplayTimeout: 3000,
+      autoplay: true,
+      smartSpeed: 950,
+  });
+  $('.owl-next').click(function() {
+    prodSlider.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.owl-prev').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    prodSlider.trigger('prev.owl.carousel', [300]);
+});
+});
